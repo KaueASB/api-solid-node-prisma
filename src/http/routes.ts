e694b523107e42cb72d31a1fa1862cfe03e2b56c
Kaue Alves <kaue_alves00@yahoo.com>
2024-01-22 18:11:10 -0300

    feat: create controller authenticate

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 
+import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register-user'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 
   app.get('/', (_, reply) => {
     return reply.send('Hello World')
