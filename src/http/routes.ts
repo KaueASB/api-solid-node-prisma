9f79dce99e28146651b60c4d18b168b55e2b0f2e
Kaue Alves <kaue_alves00@yahoo.com>
2024-01-25 15:33:33 -0300

    config: create middleware jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
+import { verifyJwt } from '@/middlewares/verify-jwt'
+
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 import { register } from './controllers/register-user'
@@ -9,7 +11,7 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate)
 
   /**  Authenticated */
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 
   /** Basic response */
   app.get('/', (_, reply) => {
