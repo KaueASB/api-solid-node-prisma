4aa4afca29dc8922cafee8762486f5c33d91928c
Kaue Alves <kaue_alves00@yahoo.com>
2024-01-25 21:15:03 -0300

    feat: controller search gym and fetch nearby gym

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,30 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeFetchNearbyGyms } from '@/use-cases/factories/make-fetch-nearby-gyms'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    page: z.coerce.number().min(1).default(1),
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude, page } = nearbyGymsQuerySchema.parse(
+    request.query,
+  )
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGyms()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+    page,
+  })
+
+  return reply.status(200).send({ gyms })
+}
